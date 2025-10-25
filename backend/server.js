b7b28ff4492268ef723072be2106ddeaaf057b72
Purv-Sonani <purv360@gmail.com>
2025-10-25 14:32:09 +0200

    Setup for Deployment

--- backend/server.js
+++ backend/server.js
@@ -3,30 +3,6 @@ import cors from "cors";
 import mongoose from "mongoose";
 import tasksRouter from "./routes/tasks.js";
 
-// const app = express();
-// const port = process.env.PORT || 5001;
-
-// // --- MIDDLEWARE ---
-// app.use(cors());
-// app.use(express.json());
-
-// // --- DATABASE CONNECTION ---
-// const uri = "mongodb+srv://purv_do_go_done:Purv%402809@dogodonecluster.3y9vus0.mongodb.net/";
-// // const uri = "mongodb+srv://temp_admin:testpass123@dogodonecluster.3y9vus0.mongodb.net/";
-
-// mongoose
-//   .connect(uri)
-//   .then(() => console.log("MongoDB connection established successfully"))
-//   .catch((err) => console.log(err));
-
-// app.use("/api/tasks", tasksRouter);
-
-// // --- API ENDPOINTS ---
-// app.get("/api/test", (req, res) => {
-//   res.json({ message: "Hello from the backend!" });
-// });
-
-// export default app;
 const app = express();
 // Remove port, Vercel handles this
 // const port = process.env.PORT || 5001;
